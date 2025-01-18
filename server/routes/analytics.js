3375dabc9f2477d7875e21177b484bc2d0ad3465
Satwika Kollati <21je0474@iitism.ac.in>
2025-01-19 03:09:36 +0530

    analytics update

--- server/routes/analytics.js
+++ server/routes/analytics.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { getUserProgress } = require("../controllers/analyticsController");
-const { authenticate } = require("../middleware/authMiddleware");
-
+const { getAnalytics } = require("../controllers/analyticsController");
+const requireAuth = require('../middleware/requireAuth'); 
 const router = express.Router();
 
-router.get("/progress", authenticate, getUserProgress);
+// router.get("/progress", authenticate, getAnalytics);
+const AnalyticsController = require("../controllers/analyticsController");
+
+// Route to get analytics data
+router.get("/",requireAuth, AnalyticsController.getAnalytics);
 
 module.exports = router;
